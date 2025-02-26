cd4cb030b8dd7758c9f69d0392c0b1bd37a1e400
CyberTropic <CyberTropic@users.noreply.github.com>
2025-02-25 23:17:45 -0500

    Update RMCPlanetMapPrototypeComponent.cs

--- Content.Shared/_RMC14/Rules/RMCPlanetMapPrototypeComponent.cs
+++ Content.Shared/_RMC14/Rules/RMCPlanetMapPrototypeComponent.cs
@@ -8,6 +8,7 @@ namespace Content.Shared._RMC14.Rules;
 [Access(typeof(RMCPlanetSystem))]
 public sealed partial class RMCPlanetMapPrototypeComponent : Component
 {
+
     [DataField(required: true), AutoNetworkedField, Access(Other = AccessPermissions.ReadExecute)]
     public ResPath Map;
 
@@ -15,8 +16,29 @@ public sealed partial class RMCPlanetMapPrototypeComponent : Component
     public CamouflageType Camouflage = CamouflageType.Jungle;
 
     [DataField, AutoNetworkedField]
+    public List<ScenarioBase> Scenarios = new();
+
+    [DataField, AutoNetworkedField]
     public int MinPlayers;
 
     [DataField(required: true), AutoNetworkedField]
     public string Announcement = string.Empty;
+
+    [DataRecord]
+    public abstract class ScenarioBase;
+
+    public sealed class ScenarioType : ScenarioBase
+    {
+        [DataField, AutoNetworkedField]
+        public string ScenarioName = string.Empty;
+
+        [DataField, AutoNetworkedField]
+        public float Probability = 0f;
+    }
+
+    public sealed class ScenarioPick : ScenarioBase
+    {
+        [DataField, AutoNetworkedField]
+        public List<ScenarioType> Pick = new();
+    }
 }
