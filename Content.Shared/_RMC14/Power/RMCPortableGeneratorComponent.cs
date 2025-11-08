91541e9fecd6e0dc713945a4840d8506468b1e56
CyberTropic <vfalach@gmail.com>
2025-11-08 00:15:54 -0500

    Create RMCPortableGeneratorComponent.cs

---
+++ Content.Shared/_RMC14/Power/RMCPortableGeneratorComponent.cs
@@ -0,0 +1,21 @@
+﻿using Content.Shared.Stacks;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Power;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(RMCPortableGeneratorSystem))]
+public sealed partial class RMCPortableGeneratorComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public bool On;
+
+    [DataField]
+    public ProtoId<StackPrototype>? Material = "CMPhoron";
+
+    [DataField]
+    public int MaxFuelUnits = 100;
+
+    [DataField]
+    public float TimePerFuelUnit = 60.0f; // Seconds per fuel unit.
+}
